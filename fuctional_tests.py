001a0c270f079a28eec0be31171c0a265b1c1e57
Conner McCarthy <chmccart@ncsu.edu>
2021-07-14 10:52:17 -0400

    First test commented and updated to use unittest.

--- fuctional_tests.py
+++ fuctional_tests.py
@@ -1,5 +1,38 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+import unittest
+class newVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # A user wants to create a list and visits the site 
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!') 
+
+        # They have an option to enter a to-do item
+
+        # They want to pick up some grocieries later and types in "buy wheat bread"
+
+        # When they hit enter the page updates, and the page lists "Buy wheat bread" in the to-do list
+
+        # On the page there is still an option to enter another item. They enter "buy ice cream".
+
+        # The page updates again, and both items are on the list
+
+        # The user wants to save the list, and the site has generated a unique URL to save
+
+        # They visit the URL and the list is the same
+
+        # They close the site
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
